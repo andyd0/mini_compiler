443225ecd7c79a183d898c00387c858d856e48bd
Andy Davila <andy.davila@gmail.com>
2020-07-08 22:04:36 -0400

    Adding frames for function calls

---
+++ src/monkey/vm/frame.go
@@ -0,0 +1,20 @@
+package vm
+
+import (
+	"monkey/code"
+	"monkey/object"
+)
+
+// ip is a pointer in this frame for this function
+type Frame struct {
+	fn *object.CompiledFunction
+	ip int
+}
+
+func NewFrame(fn *object.CompiledFunction) *Frame {
+	return &Frame{fn: fn, ip: -1}
+}
+
+func (f *Frame) Instructions() code.Instructions {
+	return f.fn.Instructions
+}
